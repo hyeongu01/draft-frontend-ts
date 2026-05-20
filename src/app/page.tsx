5d661da1bf15d25eff32639f95f6dc53fdf097e4
choihw <choihw@choihwui-MacBookPro.local>
2026-05-21 00:11:39 +0900

    feat: google 로그인 추가가

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <Button>dsfsd</Button>
+    </main>
+  );
+}
