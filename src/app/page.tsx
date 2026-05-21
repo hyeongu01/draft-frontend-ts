80c9728d4d4e6499844218e65386098193b10d5c
hyeongu01 <cdy5212@gmail.com>
2026-05-21 14:36:07 +0900

    feat: 프록시 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/lib/supabase/client";
 
 export default function Home() {
+  const supabase = createClient();
+
+  const googleLogin = () => {
+    supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+  };
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <Button>dsfsd</Button>
+      <Button onClick={googleLogin}>Google 로그인</Button>
     </main>
   );
 }
